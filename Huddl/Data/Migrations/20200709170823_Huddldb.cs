ea70737d72a78d85256bf956f9708455b61a523b
Sibusiso Tikili <sbutikili@gmail.com>
2020-07-09 20:25:00 +0200

    Add project files.

---
+++ Huddl/Data/Migrations/20200709170823_Huddldb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Huddl.Data.Migrations
+{
+    public partial class Huddldb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
